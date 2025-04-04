ccabd01198fb449a3cef3f5777bd07b94634c778
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-04 16:40:56 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -3,9 +3,9 @@ import time
 
 # Open SPI bus
 spi = spidev.SpiDev()
-spi.open(0, 0)  # Open SPI bus 0, device 0
-spi.max_speed_hz = 50000
-spi.mode = 0b00
+spi.open(0, 0)  # Open SPI bus 0, device 0 (usually the default)
+spi.max_speed_hz = 50000  # Set SPI speed
+spi.mode = 0b00  # SPI mode 0 (CPOL=0, CPHA=0)
 
 # Send a test value and receive the same value
 test_value = [0xAA]  # Sending 0xAA as an example
