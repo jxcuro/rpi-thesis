d1953e0a07f31109e2cd7b948eebb00747955799
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-04 18:24:52 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -5,19 +5,19 @@ import time
 spi = spidev.SpiDev()
 spi.open(0, 0)  # SPI bus 0, device 0 (CE0)
 
-# Set SPI parameters
-spi.max_speed_hz = 500000  # Set speed to 500kHz
+# Set SPI parameters (slower speed for testing)
+spi.max_speed_hz = 100000  # Set to 100kHz to avoid timing issues
 spi.mode = 0  # Mode 0 (CPOL=0, CPHA=0)
 
 # Send a byte and receive the same (loopback)
 print("Sending byte [0x01], expecting loopback.")
 response = spi.xfer([0x01])  # Transmit byte and get response
-print("Response:", response)  # You should see [0x01] if loopback is working
+print("Response:", response)  # Should see [0x01]
 
 # Send multiple bytes and receive data
 print("Sending bytes [0x01, 0x02, 0x03], expecting a response.")
 response = spi.xfer([0x01, 0x02, 0x03])
-print("Response:", response)  # Response should be similar to what was sent
+print("Response:", response)  # Expect [0x01, 0x02, 0x03]
 
 # Close SPI after use
 spi.close()
