4820b13d77a79d12e83e72c40398154aa88487d4
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-04 16:54:26 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -4,8 +4,8 @@ spi = spidev.SpiDev()
 spi.open(0, 0)  # Open SPI bus 0, device 0 (CE0)
 spi.max_speed_hz = 50000
 
-# Send a test byte
-response = spi.xfer2([0x00])
+# Read 1 byte from MCP3008 (or another SPI device)
+response = spi.xfer2([0x01, 0x80, 0x00])  # Example for MCP3008
 print("SPI response:", response)
 
 spi.close()
