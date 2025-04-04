8a0722e08f9e47bcf508ba73a78d4566960a8d0b
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-04 16:39:01 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -7,9 +7,7 @@ spi.open(0, 0)  # Open SPI bus 0, device 0
 spi.max_speed_hz = 50000
 spi.mode = 0b00
 
-# Test command to send over SPI
-command = [0x01, 0x00]  # You can send any test command; this is just an example
-
-# Send and receive data
-response = spi.xfer2(command)
-print(f"SPI response: {response}")
+# Send a test value and receive the same value
+test_value = [0xAA]  # Sending 0xAA as an example
+response = spi.xfer2(test_value)
+print(f"Loopback SPI response: {response}")
