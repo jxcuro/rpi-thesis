3b46ceda6e242ff4b3050fab765e9e543c7df9b4
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-05 13:37:35 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -1,2 +1,10 @@
-chip_id = read_register(0x3F)
-print(f"Chip ID: {chip_id:02X}")
+import spidev
+
+spi = spidev.SpiDev()
+spi.open(0, 0)  # bus 0, device 0
+spi.max_speed_hz = 500000
+
+def read_register(addr):
+    tx = [0x80 | addr, 0x00]  # 0x80 to set MSB for read
+    rx = spi.xfer2(tx)
+    return rx[1]
