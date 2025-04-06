2a5d7e34fc47c6294ec0600e9c281b44281aa5d1
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-06 15:50:06 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -1,32 +1,45 @@
 import spidev
 import time
+import RPi.GPIO as GPIO
+
+CS_PIN = 8  # Use GPIO8 (CE0)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CS_PIN, GPIO.OUT)
+GPIO.output(CS_PIN, GPIO.HIGH)
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 8000000
-spi.mode = 1  # CPOL=0, CPHA=1
+spi.mode = 1  # SPI Mode 1: CPOL=0, CPHA=1
+
+def spi_transfer(data):
+    GPIO.output(CS_PIN, GPIO.LOW)
+    resp = spi.xfer2(data)
+    GPIO.output(CS_PIN, GPIO.HIGH)
+    time.sleep(0.001)
+    return resp
 
 def write_register(addr, value):
-    spi.xfer2([addr & 0x7F, value])
-    time.sleep(0.001)
+    spi_transfer([addr & 0x7F, value])
 
 def read_register(addr):
-    return spi.xfer2([addr | 0x80, 0x00])[1]
+    return spi_transfer([addr | 0x80, 0x00])[1]
 
 def configure_lhr_mode():
-    write_register(0x25, 0x01)  # Power down
+    write_register(0x25, 0x01)
     time.sleep(0.01)
 
-    write_register(0x1A, 0x02)  # LHR mode
-    write_register(0x12, 0xFF)  # SETTLE_COUNT_LSB
-    write_register(0x13, 0xFF)  # SETTLE_COUNT_MSB
-    write_register(0x14, 0x03)  # CLOCK_DIVIDERS: FIN_DIV=1, FREF_DIV=1
-    write_register(0x1E, 0xFF)  # LHR_RCOUNT_LSB
-    write_register(0x1F, 0xFF)  # LHR_RCOUNT_MID
-    write_register(0x20, 0x0F)  # LHR_RCOUNT_MSB
-    write_register(0x24, 0x20)  # LHR_CONFIG with averaging
+    write_register(0x1A, 0x02)
+    write_register(0x12, 0xFF)
+    write_register(0x13, 0xFF)
+    write_register(0x14, 0x03)
+    write_register(0x1E, 0xFF)
+    write_register(0x1F, 0xFF)
+    write_register(0x20, 0x0F)
+    write_register(0x24, 0x20)
 
-    write_register(0x25, 0x00)  # Power up
+    write_register(0x25, 0x00)
     time.sleep(0.01)
 
 def dump_registers():
