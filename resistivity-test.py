c6a5e0ce34cd782983970deab0e0e8277718e0f8
jxcuro <163516589+jxcuro@users.noreply.github.com>
2025-04-07 11:34:15 +0800

    Update resistivity-test.py

--- resistivity-test.py
+++ resistivity-test.py
@@ -119,16 +119,19 @@ def enable_powermode(mode):
     write_register(START_CONFIG_REG, mode)
 
 def enable_lmode():
+    write_register(START_CONFIG_REG, SLEEP_MODE)
     write_register(ALT_CONFIG_REG, 0x01)
     write_register(D_CONF_REG, 0x01)
     write_register(START_CONFIG_REG, ACTIVE_CONVERSION_MODE)
 
 def enable_rpmode():
+    write_register(START_CONFIG_REG, SLEEP_MODE)
     write_register(ALT_CONFIG_REG, 0x02)
     write_register(D_CONF_REG, 0x00)
     write_register(START_CONFIG_REG, ACTIVE_CONVERSION_MODE)
 
 def enable_lhrmode():
+    write_register(START_CONFIG_REG, SLEEP_MODE)
     write_register(RP_SET_REG, 0x07)
     write_register(DIG_CONFIG_REG, 0xE7)
     write_register(LHR_RCOUNT_LSB_REG, 0x4A)
